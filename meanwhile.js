9ff29e276a7ed8db5737ca77e42f60107b5890da
Edu Poch <edupoch@gmail.com>
2013-04-27 14:22:31 +0200

    OO style

--- meanwhile.js
+++ meanwhile.js
@@ -1,191 +1,310 @@
 (function ($) {
 
-    var defaultOptions,
-        $videoBuffer,
+    var Video = function (options) {
+            return new Video.Instance(options);
+        },
+        Canvas = function (options) {
+            return new Canvas.Instance(options);
+        },
+        defaultOptions,
         methods,
-        downloadingVideo = [],
         downloading,
-        i,
-        r,
-        g,
-        b,
-        factorVariance = 0.4,
-        rfactor = [0.5, 0.5, 0.5],
-        gfactor = [0.5, 0.5, 0.5],
-        bfactor = [0.5, 0.5, 0.5],
-        idata,
-        brightness,
-        sepiaOpacity = 1,
-        effects = {},
-        idBuffer1,
-        idBuffer2,
-        video,
-        canvas,
-        context,
-        backCanvas,
-        backContext,
-        elementAux,
-        bufferAux,
-        drawTimeout;
+        mainVideo,
+        bufferVideo,
+        DEBUG_MODE = true;
 
-    function findColorDifference(dif, dest, src) {
-        return (dif * dest + (1 - dif) * src);
+    function log(message) {
+        if (DEBUG_MODE === true) {
+            console.log('Meanwhile.js: ' + message);
+        }
     }
 
-    function recalculateFactor(factor) {
-        // return Math.max(0, Math.min(1, factor + factorVariance * (Math.random() - 0.5)));
-        return Math.random();
-    }
+    Video.STATUS_EMPTY = 'empty';
+    Video.STATUS_DOWNLOADING = 'downloading';
+    Video.STATUS_READY = 'ready';
+    Video.STATUS_PLAYING = 'playing';
+    Video.STATUS_PAUSED = 'paused';
+    Video.STATUS_ENDED = 'ended';
 
-    function recalculateArray(f) {
-        f[0] = recalculateFactor(f[0]);
-        f[1] = recalculateFactor(f[1]);
-        f[2] = recalculateFactor(f[2]);
+    Video.Instance = function (options) {
+        var self = this;
 
-        return f;
-    }
+        this.options = {
+            width: 480,
+            height: 480,
+            id: 'video'
+        };
 
-    effects.bw = function (data) {
-
-        for (i = 0; i < data.length; i = i + 4) {
-            r = data[i];
-            g = data[i + 1];
-            b = data[i + 2];
-            brightness = (3 * r + 4 * g + b) >>> 3;
-            data[i] = brightness;
-            data[i + 1] = brightness;
-            data[i + 2] = brightness;
+        if (options) {
+            $.extend(this.options, options);
         }
 
-        return data;
+        this.element = $('<video />');
+        this.element
+            .attr({
+                'id': this.options.id,
+                'preload': 'auto',
+                'autobuffer': true
+            })
+            .css({
+                'display': 'none'
+            });
+
+        this.video = this.element.get(0);
+
+        this.status = Video.STATUS_EMPTY;
+
+        return this;
     };
 
-    effects.sepia = function (data) {
+    Video.Instance.prototype = {
+        drawOnCanvas: function (canvas) {
+            canvas.draw(this);
+        },
 
-        for (i = 0; i < data.length; i = i + 4) {
-            r = data[i];
-            g = data[i + 1];
-            b = data[i + 2];
+        load: function (sourceUrl, callback) {
+            var self = this;
 
-            data[i] = findColorDifference(sepiaOpacity, (r * 0.393) + (g * 0.769) + (b * 0.189), r);
-            data[i + 1] = findColorDifference(sepiaOpacity, (r * 0.349) + (g * 0.686) + (b * 0.168), g);
-            data[i + 2] = findColorDifference(sepiaOpacity, (r * 0.272) + (g * 0.534) + (b * 0.131), b);
-        }
+            log('loading ' + this.options.id);
 
-        return data;
-    };
+            this.status = Video.STATUS_DOWNLOADING;
 
+            $.ajax({
+                url: sourceUrl + '?buffer=' + this.options.id,
+                success: function (data) {
 
-    effects.changing = function (data) {
+                    log('success!');
 
-        for (i = 0; i < data.length; i = i + 4) {
-            r = data[i];
-            g = data[i + 1];
-            b = data[i + 2];
+                    self.element.attr('src', self.options.id + '.mp4');
+                    self.status = Video.STATUS_READY;
 
-            data[i] = findColorDifference(sepiaOpacity, (r * rfactor[0]) + (g * rfactor[1]) + (b * rfactor[2]), r);
-            data[i + 1] = findColorDifference(sepiaOpacity, (r * gfactor[0]) + (g * gfactor[1]) + (b * gfactor[2]), g);
-            data[i + 2] = findColorDifference(sepiaOpacity, (r * bfactor[0]) + (g * bfactor[1]) + (b * bfactor[2]), b);
-        }
+                    if (callback) {
+                        callback();
+                    }
 
-        return data;
-    };
+                    self = null;
+                }
+            });
+        },
 
-    function draw(video, context, backContext, width, height) {
-        if (!!drawTimeout) {
-            clearTimeout(drawTimeout);
-        }
+        play: function (callback) {
+            var self = this;
+            this.status = Video.STATUS_PLAYING;
+            this.video.play();
 
-        if (video.paused || video.ended) { return false; }
-
-        backContext.drawImage(video, 0, 0, width, height);
-
-        idata = backContext.getImageData(0, 0, width, height);
-        idata.data = effects.changing(idata.data);
-
-        context.putImageData(idata, 0, 0);
-        // Start over!        
-        drawTimeout = setTimeout(draw, 20, video, context, backContext, width, height);
-    }
-
-    function loadVideo(sourceUrl, video, callback) {
-        idBuffer1 = video.attr('id');
-
-        console.log('loading...');
-
-        downloadingVideo[idBuffer1] = true;
-
-        $.ajax({
-            url: sourceUrl + '?buffer=' + idBuffer1,
-            success: function (data) {
-
-                console.log('success!');
-
-                video.attr('src', idBuffer1 + '.mp4');
-
-                downloadingVideo[idBuffer1] = false;
-
-                if (callback) {
+            this.element.bind("ended", function () {
+                self.status = Video.STATUS_ENDED;
+                self.element.unbind("ended");
+                self = null;
+                if (!!callback) {
                     callback();
                 }
+            });
+        },
+
+        restart: function () {
+            this.element.unbind('ended');
+            this.status = Video.STATUS_PAUSED;
+            this.video.pause();
+            this.video.currentTime = 0;
+        },
+
+        id: function () {
+            return this.options.id;
+        },
+
+        width: function () {
+            return this.options.width;
+        },
+
+        height: function () {
+            return this.options.height;
+        }
+    };
+
+    Canvas.Instance = function (options) {
+        var self = this;
+
+        this.options = {
+            effect: 'changing',
+            effectOpacity: 1,
+            width: 480,
+            height: 480
+        };
+
+        if (options) {
+            $.extend(this.options, options);
+        }
+
+        this.element = $('<canvas />');
+        this.backCanvas = this.element.clone();
+
+        this.element
+            .css({
+                'position': 'absolute',
+                'top': 0,
+                'left': 0,
+                'display': 'block',
+                'width': '100%'
+            });
+
+        this.element.get(0).width = this.options.width;
+        this.element.get(0).height = this.options.height;
+        this.backCanvas.get(0).width = this.options.width;
+        this.backCanvas.get(0).height = this.options.height;
+
+        this.context = this.element.get(0).getContext('2d');
+        this.backContext = this.backCanvas.get(0).getContext('2d');
+        this.timeout = undefined;
+
+        return this;
+    };
+
+    Canvas.Instance.prototype = {
+        draw: function (video) {
+            this.recalculateEffectFactors();
+            Canvas.utils.drawOneVideo(video, this.context, this.backContext, this.options);
+        },
+
+        recalculateEffectFactors: function () {
+            switch (this.options.effect) {
+
+            case 'changing':
+                Canvas.aux.rfactor = Canvas.utils.recalculateFactorArray(Canvas.aux.rfactor);
+                Canvas.aux.gfactor = Canvas.utils.recalculateFactorArray(Canvas.aux.gfactor);
+                Canvas.aux.bfactor = Canvas.utils.recalculateFactorArray(Canvas.aux.bfactor);
+                break;
             }
-        });
-    }
-
-    function populateVideo(sourceUrl, video) {
-        // window.setTimeout(function () {
-        loadVideo(sourceUrl, video);
-        // }, 2000);
-    }
-
-    function showVideo(sourceUrl, $element, $buffer, $canvas, $backCanvas) {
-        console.log('showing...');
-
-        downloading = downloadingVideo[$element.attr('id')];
-        if (downloading) {
-            console.log('wait! it\'s still downloading');
         }
-        elementAux = downloading ? $buffer : $element;
-        bufferAux = downloading ? $element : $buffer;
+    };
 
-        idBuffer2 = elementAux.attr('id');
-        video = elementAux.get(0);
-        canvas = $canvas.get(0);
-        context = canvas.getContext('2d');
-        backCanvas = $backCanvas.get(0);
-        backContext = backCanvas.getContext('2d');
+    Canvas.timeout = undefined;
 
-        canvas.width = 480;
-        canvas.height = 480;
+    Canvas.utils = {
+        findColorDifference: function (dif, dest, src) {
+            return (dif * dest + (1 - dif) * src);
+        },
 
-        backCanvas.width = canvas.width;
-        backCanvas.height = canvas.height;
+        recalculateFactor: function (factor) {
+            // return Math.max(0, Math.min(1, factor + factorVariance * (Math.random() - 0.5)));
+            return Math.random();
+        },
 
-        rfactor = recalculateArray(rfactor);
-        gfactor = recalculateArray(gfactor);
-        bfactor = recalculateArray(bfactor);
+        recalculateFactorArray: function (f) {
+            f[0] = Canvas.utils.recalculateFactor(f[0]);
+            f[1] = Canvas.utils.recalculateFactor(f[1]);
+            f[2] = Canvas.utils.recalculateFactor(f[2]);
 
-        console.log('playing ' + idBuffer2);
+            return f;
+        },
+
+        drawOneVideo: function (video, context, backContext, options) {
+            if (!!Canvas.timeout) {
+                clearTimeout(Canvas.timeout);
+            }
+
+            if (video.status === Video.STATUS_PAUSED || video.status === Video.STATUS_ENDED) {
+                return false;
+            }
+
+            backContext.drawImage(video.video, 0, 0, video.width(), video.height());
+            Canvas.aux.idata = backContext.getImageData(0, 0, video.width(), video.height());
+            Canvas.aux.idata.data = Canvas.effects[options.effect](Canvas.aux.idata.data, options);
+
+            context.putImageData(Canvas.aux.idata, 0, 0);
+            // Start over!        
+            Canvas.timeout = setTimeout(Canvas.utils.drawOneVideo, 20, video, context, backContext, options);
+        }
+    };
+
+    Canvas.aux = {
+        i: 0,
+        r: 0,
+        g: 0,
+        b: 0,
+        brightness: 0,
+        idata: null,
+        factorVariance: 0.4,
+        rfactor: [0.5, 0.5, 0.5],
+        gfactor: [0.5, 0.5, 0.5],
+        bfactor: [0.5, 0.5, 0.5]
+    };
+
+    Canvas.effects = {
+        greyscale: function (data, options) {
+
+            for (Canvas.aux.i = 0; Canvas.aux.i < data.length; Canvas.aux.i = Canvas.aux.i + 4) {
+                Canvas.aux.r = data[Canvas.aux.i];
+                Canvas.aux.g = data[Canvas.aux.i + 1];
+                Canvas.aux.b = data[Canvas.aux.i + 2];
+
+                Canvas.aux.brightness = (3 * Canvas.aux.r + 4 * Canvas.aux.g + Canvas.aux.b) >>> 3;
+
+                data[Canvas.aux.i]     = Canvas.aux.brightness;
+                data[Canvas.aux.i + 1] = Canvas.aux.brightness;
+                data[Canvas.aux.i + 2] = Canvas.aux.brightness;
+            }
+
+            return data;
+        },
+
+        sepia: function (data, options) {
+
+            for (Canvas.aux.i = 0; Canvas.aux.i < data.length; Canvas.aux.i = Canvas.aux.i + 4) {
+                Canvas.aux.r = data[Canvas.aux.i];
+                Canvas.aux.g = data[Canvas.aux.i + 1];
+                Canvas.aux.b = data[Canvas.aux.i + 2];
+
+                data[Canvas.aux.i]     = Canvas.utils.findColorDifference(options.effectOpacity, (Canvas.aux.r * 0.393) + (Canvas.aux.g * 0.769) + (Canvas.aux.b * 0.189), Canvas.aux.r);
+                data[Canvas.aux.i + 1] = Canvas.utils.findColorDifference(options.effectOpacity, (Canvas.aux.r * 0.349) + (Canvas.aux.g * 0.686) + (Canvas.aux.b * 0.168), Canvas.aux.g);
+                data[Canvas.aux.i + 2] = Canvas.utils.findColorDifference(options.effectOpacity, (Canvas.aux.r * 0.272) + (Canvas.aux.g * 0.534) + (Canvas.aux.b * 0.131), Canvas.aux.b);
+            }
+
+            return data;
+        },
+
+        changing: function (data, options) {
+
+            for (Canvas.aux.i = 0; Canvas.aux.i < data.length; Canvas.aux.i = Canvas.aux.i + 4) {
+                Canvas.aux.r = data[Canvas.aux.i];
+                Canvas.aux.g = data[Canvas.aux.i + 1];
+                Canvas.aux.b = data[Canvas.aux.i + 2];
+
+                data[Canvas.aux.i]     = Canvas.utils.findColorDifference(options.effectOpacity, (Canvas.aux.r * Canvas.aux.rfactor[0]) + (Canvas.aux.g * Canvas.aux.rfactor[1]) + (Canvas.aux.b * Canvas.aux.rfactor[2]), Canvas.aux.r);
+                data[Canvas.aux.i + 1] = Canvas.utils.findColorDifference(options.effectOpacity, (Canvas.aux.r * Canvas.aux.gfactor[0]) + (Canvas.aux.g * Canvas.aux.gfactor[1]) + (Canvas.aux.b * Canvas.aux.gfactor[2]), Canvas.aux.g);
+                data[Canvas.aux.i + 2] = Canvas.utils.findColorDifference(options.effectOpacity, (Canvas.aux.r * Canvas.aux.bfactor[0]) + (Canvas.aux.g * Canvas.aux.bfactor[1]) + (Canvas.aux.b * Canvas.aux.bfactor[2]), Canvas.aux.b);
+            }
+
+            return data;
+        }
+    };
+
+    function videoLoop(sourceUrl, video1, video2, canvas) {
+        log('showing ' + video1.id());
+
+        downloading = video1.status === Video.STATUS_DOWNLOADING;
 
         if (downloading) {
-            elementAux.unbind('ended');
-            video.pause();
-            video.currentTime = 0;
+            log('\twait! it\'s still downloading');
+        }
+        mainVideo = downloading ? video2 : video1;
+        bufferVideo = downloading ? video1 : video2;
+
+        if (downloading) {
+            mainVideo.restart();
         }
 
-        video.play();
-        draw(video, context, backContext, canvas.width, canvas.height);
+        log('playing ' + mainVideo.id());
 
-        elementAux.bind("ended", function () {
-            elementAux.unbind("ended");
-            console.log(idBuffer2 + ' ended');
-            showVideo(sourceUrl, bufferAux, elementAux, $canvas, $backCanvas);
+        mainVideo.play(function () {
+            log(mainVideo.id() + ' ended');
+            videoLoop(sourceUrl, bufferVideo, mainVideo, canvas);
         });
+        canvas.draw(mainVideo);
 
         if (!downloading) {
-            populateVideo(sourceUrl, bufferAux);
+            bufferVideo.load(sourceUrl);
         }
-
     }
 
     defaultOptions = {
@@ -201,39 +320,18 @@
 
             return this.each(function () {
                 var $this = $(this),
-                    $video = $('<video>'),
-                    $canvas = $('<canvas/>'),
-                    $backCanvas = $('<canvas/>');
+                    video1 = new Video({
+                        id: 'video_1'
+                    }),
+                    video2 = new Video({
+                        id: 'video_2'
+                    }),
+                    canvas = new Canvas();
 
-                $video
-                    .css({
-                        'display': 'none'
-                    })
-                    .attr({
-                        'id': 'buffer_1',
-                        'preload': 'auto',
-                        'autobuffer': true
-                    });
+                $this.append(video1.element).append(video2.element).append(canvas.element);
 
-                $canvas
-                    .css({
-                        'position': 'absolute',
-                        'top': 0,
-                        'left': 0,
-                        'display': 'block',
-                        'width': '100%'
-                    });
-
-                $videoBuffer = $video.clone();
-                $videoBuffer.attr('id', 'buffer_2');
-
-                downloadingVideo[$video.attr('id')] = false;
-                downloadingVideo[$videoBuffer.attr('id')] = false;
-
-                $this.append($video).append($videoBuffer).append($canvas);
-
-                loadVideo(options.sourceUrl, $video, function () {
-                    showVideo(options.sourceUrl, $video, $videoBuffer, $canvas, $backCanvas);
+                video1.load(options.sourceUrl, function () {
+                    videoLoop(options.sourceUrl, video1, video2, canvas);
                 });
             });
         }
